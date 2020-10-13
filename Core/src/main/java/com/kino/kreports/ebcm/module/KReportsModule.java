243a50a840c553694b8b978b12507d0cec81ec95
KinooDev <eugetcheve@gmail.com>
2020-10-13 15:58:42 +0100

    Added a new Parameter Provider for EBCM.

--- Core/src/main/java/com/kino/kreports/ebcm/module/KReportsModule.java
+++ Core/src/main/java/com/kino/kreports/ebcm/module/KReportsModule.java
@@ -1,8 +1,10 @@
 package com.kino.kreports.ebcm.module;
 
 import com.kino.kreports.ebcm.provider.ReportPriorityProvider;
+import com.kino.kreports.ebcm.provider.ReportStateProvider;
 import com.kino.kreports.ebcm.provider.UUIDProvider;
 import com.kino.kreports.utils.ReportPriority;
+import com.kino.kreports.utils.ReportState;
 import me.fixeddev.ebcm.parameter.provider.ParameterProviderRegistry;
 import me.fixeddev.ebcm.parameter.provider.ProvidersModule;
 
@@ -13,5 +15,6 @@ public class KReportsModule implements ProvidersModule {
     public void configure(ParameterProviderRegistry registry) {
         registry.registerParameterProvider(UUID.class, new UUIDProvider());
         registry.registerParameterProvider(ReportPriority.class, new ReportPriorityProvider());
+        registry.registerParameterProvider(ReportState.class, new ReportStateProvider());
     }
 }
---
+++ Core/src/main/java/com/kino/kreports/ebcm/provider/ReportStateProvider.java
@@ -0,0 +1,18 @@
+package com.kino.kreports.ebcm.provider;
+
+import com.kino.kreports.utils.ReportState;
+import me.fixeddev.ebcm.NamespaceAccesor;
+import me.fixeddev.ebcm.parameter.provider.SingleArgumentProvider;
+import me.fixeddev.ebcm.part.CommandPart;
+
+public class ReportStateProvider implements SingleArgumentProvider<ReportState> {
+
+    @Override
+    public Result<ReportState> transform(String s, NamespaceAccesor namespaceAccesor, CommandPart commandPart) {
+        try {
+            return Result.createResult(ReportState.valueOf(s.toUpperCase()));
+        } catch (IllegalArgumentException ex) {
+            return Result.createResultOfMessage("Invalid state", s);
+        }
+    }
+}
